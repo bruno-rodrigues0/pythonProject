edbcf6720231131e8974257845f47c54b8016bf9
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-04 15:04:08 -0300

    shuffle array ex

--- ex009.py
+++ ex009.py
@@ -1,8 +1,7 @@
 import random
 
 alunos = ['joao', 'maria',  'jose', 'madalena']
-seq = []
 
-for i in range(3):
-    seq[i] = random.randint(0,3)
-    print("{}, ".format(seq[i]))    
+random.shuffle(alunos)
+
+print("{}".format(alunos))
